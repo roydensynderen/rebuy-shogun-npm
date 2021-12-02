a6b827e5e9ca3616747c5fed017f26e13d282308
roydensynderen <roydensynderen@gmail.com>
2021-12-01 16:58:40 -0800

    up ver

--- dist/rebuy-data/use-rebuy-data.d.ts
+++ dist/rebuy-data/use-rebuy-data.d.ts
@@ -1,1 +1,1 @@
-export declare function useRebuyData(dataSourceId: any, apiKey: any): {};
+export declare function useRebuyData(dataSourceUrl: any): {};
--- dist/rebuy-data/use-rebuy-data.js
+++ dist/rebuy-data/use-rebuy-data.js
@@ -2,9 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useRebuyData = void 0;
 const react_1 = require("react");
-function useRebuyData(dataSourceId, apiKey) {
+function useRebuyData(dataSourceUrl) {
     const [rebuyData, setRebuyData] = (0, react_1.useState)({});
-    const dataSourceUrl = `https://cdn.rebuyengine.com/api/v1/custom/id/${dataSourceId}?key=${apiKey}`;
     (0, react_1.useEffect)(() => {
         fetch(dataSourceUrl)
             .then(response => response.json())
